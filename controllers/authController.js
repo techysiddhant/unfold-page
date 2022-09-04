36efdaf893755ed5a05e1a2fb6d602486e918bce
Siddhant Jain <68164152+techysiddhant@users.noreply.github.com>
2022-09-04 23:22:01 +0530

    Update authController.js
    Add home Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,9 @@ const createToken = (id) => {
         expiresIn: maxAge
     });
 }
+module.exports.home_get = (req, res) => {
+    res.render('home');
+}
 module.exports.login_get = (req, res) => {
     res.render('login');
 }
