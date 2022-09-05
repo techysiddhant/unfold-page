fcd3556952355d5bf8feee89eb9f4337254e7e5d
techysiddhant <sidjain8000@gmail.com>
2022-09-06 00:47:49 +0530

    apply protected middleware

--- router/authRouter.js
+++ router/authRouter.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { validateUser, userValidation } = require('../middlewares/authMiddleware');
+const { requireAuth, checkUser } = require('../middlewares/authUserVerifyMiddleware');
 
 router.get('/login', authController.login_get);
 router.post('/login', validateUser, authController.login_post);
 router.get('/signup', authController.signup_get);
 router.post('/signup', validateUser, userValidation, authController.signup_post);
 
-router.get('/', authController.home_get);
+router.get('/', requireAuth, authController.home_get);
 
 
 module.exports = router;
