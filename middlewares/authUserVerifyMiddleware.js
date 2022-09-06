5ce95443d89909af329c45c5e12a503f80367a8d
techysiddhant <sidjain8000@gmail.com>
2022-09-06 06:07:17 +0530

    change in middleware

--- middlewares/authUserVerifyMiddleware.js
+++ middlewares/authUserVerifyMiddleware.js
@@ -12,35 +12,54 @@ const requireAuth = (req, res, next) => {
                 res.redirect('/login');
             } else {
                 console.log(decodedToken);
+                // res.render('home');
                 next();
             }
         })
     } else {
-        res.redirect('/login')
+        res.redirect('/login');
     }
 }
 
 //check user
-const checkUser = (req, res, next) => {
-    const token = req.cookies.jwt;
-    if (token) {
-        jwt.verify(token, process.env.JWT_SECRET, async(err, decodedToken) => {
-            if (err) {
-                console.log(err.message);
-                res.locals.user = null
+// const checkUser = (req, res, next) => {
+//     const token = req.cookies.jwt;
+//     if (token) {
+//         jwt.verify(token, process.env.JWT_SECRET, async(err, decodedToken) => {
+//             if (err) {
+//                 console.log(err.message);
+//                 res.locals.user = null
 
-                next();
-            } else {
-                console.log(decodedToken);
-                let user = await User.findById(decodedToken.id);
-                res.locals.user = user;
-                next();
-            }
-        });
-    } else {
-        res.locals.user = null
-        next();
-    }
-}
+//                 next();
+//             } else {
+//                 console.log(decodedToken);
+//                 let user = await User.findById(decodedToken.id);
+//                 res.locals.user = user;
+//                 next();
+//             }
+//         });
+//     } else {
+//         res.locals.user = null
+//         next();
+//     }
+// }
+// const ensureGuest = (req, res, next) => {
+//     const token = req.cookies.jwt;
+//     if (token) {
+//         jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+//             if (err) {
+//                 console.log(err);
+//                 res.redirect('/login');
+//                 // next();
+//             } else {
+//                 res.redirect('/');
+//                 next();
 
-module.exports = { requireAuth, checkUser };
+//             }
+//         })
+//     } else {
+//         res.render('login');
+//         // next();
+//     }
+// }
+module.exports = { requireAuth };
