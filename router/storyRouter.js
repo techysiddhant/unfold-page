dd23e997980ac86d62ae01e2897b6a1f2ef86e58
techysiddhant <sidjain8000@gmail.com>
2022-09-08 20:41:28 +0530

    add upload image page

--- router/storyRouter.js
+++ router/storyRouter.js
@@ -6,7 +6,7 @@ const storyController = require('../controllers/storyController');
 
 router.get('/add/story', storyController.addstory_get);
 router.post('/add/story', storyController.addstory_post);
-
+router.get('/add/story/image/:id', storyController.addstoryimage_get);
 
 
 
