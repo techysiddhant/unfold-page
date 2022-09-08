bba6155e48c03d0af3b8d530a48e2f2a8b4d1c38
techysiddhant <sidjain8000@gmail.com>
2022-09-09 01:18:47 +0530

    adding image upload functionality backend

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User');
 const Story = require('../models/Story');
 const jwt = require('jsonwebtoken');
+const Grid = require("gridfs-stream");
+const mongoose = require("mongoose");
 module.exports.addstory_get = (req, res) => {
     res.render('addstory');
 }
@@ -30,4 +32,18 @@ module.exports.addstory_post = async(req, res) => {
 
 module.exports.addstoryimage_get = (req, res) => {
     res.render('image');
+}
+
+module.exports.addstoryimage_post = async(req, res) => {
+    const id = req.params.id;
+    console.log("🚀 ~ file: storyController.js ~ line 39 ~ module.exports.addstoryimage_post=async ~ id", id)
+    if (req.file === undefined) return res.send("you must select a file.");
+    try {
+        const imgid = req.file.id.toString();
+        console.log("🚀 ~ file: storyController.js ~ line 43 ~ module.exports.addstoryimage_post=async ~ imgid", imgid)
+        const story = await Story.findByIdAndUpdate({ _id: id }, { imageId: imgid });
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
 }
